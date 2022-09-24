7ef5738d27023f6468f6f367c52208f7f677d53c
ascendxrr <112617626+ascendxrr@users.noreply.github.com>
2022-09-25 06:34:58 +0800

    other number operations

---
+++ 01_numbers-operators-comments/otherNumberOperations.py
@@ -0,0 +1,14 @@
+# exponentiation
+print(2**3)
+print(4**8)
+print(49**0.5)
+
+# remainder/modulo
+# just return the remainder of this division
+# mostly used if number is even (divisible by 2 with 0 remainder) or odd
+print(10 % 3)
+print(25 % 4)
+
+print(10 / 3)
+# using // will give the value of the division without the float
+print(10 // 3)
