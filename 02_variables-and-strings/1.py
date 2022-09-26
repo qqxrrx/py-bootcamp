c88bf53518077310753f66543571f94e00d260c1
ascendxrr <112617626+ascendxrr@users.noreply.github.com>
2022-09-27 03:51:34 +0800

    'None' value

--- 02_variables-and-strings/1.py
+++ 02_variables-and-strings/1.py
@@ -7,3 +7,11 @@ print(num_of_cats)
 a, b, c = 1, 2, 3
 
 print(a, b, c)
+
+myVariable = None
+print(myVariable)
+print(type(myVariable))
+
+myVariable = "this is the new value"
+print(myVariable)
+print(type(myVariable))
