4ca0d4f7fc866fe90a4fbcc6abe0c443ed9ff831
ascendxrr <112617626+ascendxrr@users.noreply.github.com>
2022-09-27 03:58:18 +0800

    single and double quotes (strings)

--- 02_variables-and-strings/1.py
+++ 02_variables-and-strings/1.py
@@ -1,17 +1,24 @@
 num_of_cats = 0
 print(num_of_cats)
 
+
 num_of_cats = "j"
 print(num_of_cats)
 
-a, b, c = 1, 2, 3
 
+a, b, c = 1, 2, 3
 print(a, b, c)
+
 
 myVariable = None
 print(myVariable)
 print(type(myVariable))
 
+
 myVariable = "this is the new value"
 print(myVariable)
 print(type(myVariable))
+
+
+print("so 'that' was there")
+print('we found the "thing"')
