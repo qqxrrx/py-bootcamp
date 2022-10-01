e8837d940445b0dc380a53f35134647e4cb5e6f5
ascendxrr <112617626+ascendxrr@users.noreply.github.com>
2022-10-01 15:11:51 +0800

    while loop

---
+++ 04_looping/3.1.py
@@ -0,0 +1,6 @@
+msg = input("secret pass?: ")
+
+while msg != "bananas":
+    print("wrong")
+    msg = input("secret pass?: ")
+print("correct")
---
+++ 04_looping/3.2.py
@@ -0,0 +1,10 @@
+print("for-loop")
+for num in range(1, 11):
+    print(num)
+
+
+print("while-loop")
+num = 1
+while num < 11:
+    print(num)
+    num += 1
