e4708b93b9b71b25b5d427a2fbe84b5851018900
ascendxrr <112617626+ascendxrr@users.noreply.github.com>
2022-10-03 11:46:17 +0800

    list methods: append, insert, extend

---
+++ 05_lists/5.py
@@ -0,0 +1,22 @@
+# .append() = add item to end of list
+# just like Array.prototype.push() of javascript
+items = [1, 2, 3]
+items.append(4)
+print(items)
+
+
+# .extend() = add at the end of list all values
+# just like Array.prototype.concat() of javascript but changes the actual list
+items.extend([5, 6, 7])
+print(items)
+
+
+# .insert(<zero-based-index>,<data>) = insert at given position
+items.insert(4, "yo")
+print(items)
+
+
+# note = if you use -1 to insert at end of list it will use the old length, therefore will be inserted at 2nd last
+# the solution is:
+items.insert(len(items), "end of list")
+print(items)
