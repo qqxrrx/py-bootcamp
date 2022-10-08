c747d98d203fa80e601c666e1c0b4a71d76110d1
ascendxrr <112617626+ascendxrr@users.noreply.github.com>
2022-10-08 17:33:05 +0800

    map

---
+++ 09_lambdas-builtin-functions/map.py
@@ -0,0 +1,15 @@
+nums = [1, 2, 3, 4, 5]
+
+doubles = list(map(lambda n: n*2, nums))
+print(doubles)
+
+members = ['a', 'b', 'c']
+member2 = list(map(lambda n: n.upper(), members))
+print(member2)
+
+names = [{'fn': 'A1', 'ln': 'A2'},
+         {'fn': 'B1', 'ln': 'B2'},
+         {'fn': 'B1', 'ln': 'B2'}]
+
+fns = list(map(lambda n: n['fn'], names))
+print(fns)
