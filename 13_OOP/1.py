2be1e347ce6ed3bd53d97037f1528087f8df5f29
ascendxrr <112617626+ascendxrr@users.noreply.github.com>
2022-10-13 09:19:00 +0800

    class attributes

--- 13_OOP/1.py
+++ 13_OOP/1.py
@@ -1,8 +1,18 @@
 class User:
+
+    # class attribute that exists across all instances
+    active_users = 0
+
     def __init__(self, first, last, age):
+        myvariable = 0  # this is scoped within this block only
         self.first = first
         self.last = last
         self.age = age
+        User.active_users += 1
+
+    def logout(self):
+        User.active_users -= 1
+        return f"{self.first} has logged out."
 
     def full_name(self):
         return f"{self.first} {self.last}"
@@ -25,7 +35,9 @@ class User:
 
 
 user1 = User("john", "doe", 65)
+print(user1.active_users)
 user2 = User("alyssa", "smith", 18)
+print(user2.active_users)
 print(f"{user1.likes('fresh cookies')}")
 print(f"{user2.likes('warm milk')}")
 
@@ -34,3 +46,8 @@ print(f"{user2.initials()} {user2.birthday()} -- senior? {user2.is_senior()}")
 
 user1.say_hi()
 user2.say_hi()
+
+user1.logout()
+print(user1.active_users)
+user2.logout()
+print(user1.active_users)
