dc000150f01dc6d3cba3221042bd57b97fbf6894
ascendxrr <112617626+ascendxrr@users.noreply.github.com>
2022-10-13 16:15:46 +0800

    class methods (advanced)

--- 13_OOP/3.py
+++ 13_OOP/3.py
@@ -7,18 +7,27 @@ class User:
         # cls = class and not instance (self)
         return f"There are {cls.active_users} active users"
 
-    def __init__(self, name):
+    @classmethod
+    def from_string(cls, data_str):
+        name, id = data_str.split(',')
+        return cls(name, int(id))  # new class as the return value
+
+    def __init__(self, name, id):
         self.name = name
+        self.id = id
         User.active_users += 1
 
     def logout(self):
         User.active_users -= 1
 
 
-user1 = User("A")
-user2 = User("B")
+user1 = User("A", 1)
+user2 = User("B", 2)
 print(User.display_active_users())
 
-user3 = User("C")
-user4 = User("D")
+user3 = User("C", 3)
+user4 = User("D", 4)
+print(User.display_active_users())
+
+user5 = User.from_string("A,99")
 print(User.display_active_users())
