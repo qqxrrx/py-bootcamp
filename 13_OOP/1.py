a9213ea9acbb7905c74ca63839da153125473c2f
ascendxrr <112617626+ascendxrr@users.noreply.github.com>
2022-10-13 08:49:09 +0800

    __init__ method of a class

--- 13_OOP/1.py
+++ 13_OOP/1.py
@@ -1,8 +1,11 @@
 class User:
-    pass
+    def __init__(self, first, last, age):
+        self.first = first
+        self.last = last
+        self.age = age
 
 
-user1 = User()
-
-print(type(user1))
-print(user1)
+user1 = User("john", "doe", 45)
+user2 = User("alyssa", "smith", 18)
+print(f"{user1.first} {user1.last}, {user1.age} years old")
+print(f"{user2.first} {user2.last}, {user2.age} years old")
