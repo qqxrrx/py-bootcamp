315ea0f4d6ca4a833915e63df511cd96dae69302
ascendxrr <112617626+ascendxrr@users.noreply.github.com>
2022-10-25 15:45:12 +0800

    webscraping project (bootcamp answer, part 2)

--- 00_projects/webscraping-project_bootcamp-answer.py
+++ 00_projects/webscraping-project_bootcamp-answer.py
@@ -2,6 +2,8 @@
 import requests
 from bs4 import BeautifulSoup
 from time import sleep
+from random import choice
+
 
 all_quotes = []
 base_url = "https://quotes.toscrape.com"
@@ -26,5 +28,36 @@ while url:
     # --- wait 2 seconds before next, to prevent overloading server or attracting attention ---
     # sleep(2)
 
-print(all_quotes)
-print(len(all_quotes))
+quote = choice(all_quotes)
+remaining_guesses = 4
+print("Here's a quote: ")
+print(quote["text"])
+
+guess = ''
+
+while guess.lower() != quote["author"].lower() and remaining_guesses > 0:
+    guess = input(
+        f"Who said this quote? Guesses remaining: {remaining_guesses} \n")
+
+    if guess.lower() == quote['author'].lower():
+        print("YOU GOT IT RIGHT!")
+        break
+
+    remaining_guesses -= 1
+    if remaining_guesses == 3:
+        res = requests.get(f"{base_url}{quote['bio-link']}")
+        soup = BeautifulSoup(res.text, "html.parser")
+        birth_date = soup.find(class_="author-born-date").get_text()
+        birth_place = soup.find(class_="author-born-location").get_text()
+        print(
+            f"Here's a hint: The author was born on {birth_date} {birth_place}")
+    elif remaining_guesses == 2:
+        print(
+            f"Here's a hint: The author's first name starts with: {quote['author'][0]}")
+    elif remaining_guesses == 1:
+        last_initial = quote['author'].split(" ")[1][0]
+        print(
+            f"Here's a hint: The author's last name starts with: {last_initial}")
+    else:
+        print(
+            f"Sorry you ran out of guesses. The answer was {quote['author']}")
